8ad440a9e64c33c238968e1f2a46995e42f05246
Oleh Liubchenko <doroleijn@gmail.com>
2023-08-24 14:41:23 +0200

    fix

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// Виконується після завантаження сторінки
 window.addEventListener('load', function() {
+    // Отримуємо поточну дату та час
     var currentDate = new Date();
+    
+    // Вказуємо дату народження
     var birthDate = new Date("2005-10-27");
-    var timeDiff = currentDate - birthDate;
-    var age = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
+    
+    // Визначаємо вік на основі років
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    // Створюємо дату народження в поточному році
+    var birthDateThisYear = new Date(currentDate.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+
+    // Перевіряємо, чи настав або минув день народження у поточному році
+    if (currentDate < birthDateThisYear) {
+        age--; // Зменшуємо вік, якщо день народження ще не настав у поточному році
+    }
+
+    // Вставляємо обчислений вік у відповідний елемент на сторінці
     document.getElementById("age").textContent = age;
 });
