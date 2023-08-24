23b363996f1678e931bcce93551df7c6766481d9
Oleh Liubchenko <doroleijn@gmail.com>
2023-08-24 14:19:29 +0200

    fix

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function calculateAge() {
-    const birthdateInput = document.getElementById("birthdate");
-    const resultElement = document.getElementById("result");
-
-    const birthdate = new Date(birthdateInput.value);
-    const currentDate = new Date();
-
-    const years = currentDate.getFullYear() - birthdate.getFullYear();
-    const months = currentDate.getMonth() - birthdate.getMonth();
-    const days = currentDate.getDate() - birthdate.getDate();
-
-    if (months < 0 || (months === 0 && days < 0)) {
-        years--;
-    }
-
-    resultElement.textContent = `Вік: ${years} років`;
-}
+window.addEventListener('load', function() {
+    var currentDate = new Date();
+    var birthDate = new Date("2005-10-27");
+    var timeDiff = currentDate - birthDate;
+    var age = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
+    document.getElementById("age").textContent = age;
+});
